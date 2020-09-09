54e60e6fa3622ec31a7ed469fdf189524a328a32
Artem Maksimov <labratramzan@gmail.com>
2020-09-09 05:28:41 +0300

    refactor migrations

--- src/main/resources/db/migration/v0.1.sql
+++ src/main/resources/db/migration/v0.1.sql
@@ -3,8 +3,8 @@
 --changeset ma:v0.1
 create table clients
 (
-    id      uuid        primary key,
-    name    varchar(128)
+    id      uuid            primary key,
+    name    varchar(128)    not null
 );
 
 create table debts
@@ -12,16 +12,11 @@ create table debts
     id      uuid        primary key,
     value   numeric     not null,
     client_id   uuid    not null,
-    constraint client_id_fk foreign key(client_id) references clients on delete cascade,
-    constraint value_gt_zero check (value>0)
+    constraint debts_client_id_fk foreign key(client_id) references clients on delete cascade,
+    constraint debts_value_gt_zero check (value>0)
 );
 
 --changeset ma:v0.1.1
-alter table debts drop constraint value_gt_zero;
-
-alter table debts
-    add constraint debts_value_gt_0 check (value>0);
-
 alter table debts
     add column version integer not null default 0;
 
@@ -30,11 +25,6 @@ create table payments
     id      uuid        primary key,
     value   numeric     not null,
     debt_id uuid        not null,
-    constraint debt_id_fk foreign key(debt_id) references debts on delete cascade,
+    constraint payments_debt_id_fk foreign key(debt_id) references debts on delete cascade,
     constraint payments_value_gt_0 check (value>0)
 );
-
---changeset ma:v0.1.2
-alter table clients
-    add constraint clients_name_len_gt_0 check (length(name)>1);
-
